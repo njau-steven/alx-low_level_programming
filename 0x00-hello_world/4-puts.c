1ce7be27f9e7316dd7f99e718ee4e6a5c71893e3
Njau Steven <njau.steven@gmail.com>
2022-12-09 15:57:05 -0800

    Puts output function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * Description: Display using puts function
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
