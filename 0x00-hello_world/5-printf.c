ed6aa809c5676bbd8893ef2c58ed7e69a2099e54
Njau Steven <njau.steven@gmail.com>
2022-12-09 02:49:37 -0800

    Display using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *Code to print using printf function
+ * main function void
+ * return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
