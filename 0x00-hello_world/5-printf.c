6c4d56626e379345d39ce0cf6bd6b2b67f0e2659
Njau Steven <njau.steven@gmail.com>
2022-12-09 02:58:36 -0800

    Display using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /*
- * main - Entry point
+ * main function void - Entry point
  * return 0
  */
 
-int main(void)
+int main(void) 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
