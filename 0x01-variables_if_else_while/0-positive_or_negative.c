02b0868a524271d5c59aacdf1103db07bb79e7ea
Njau Steven <njau.steven@gmail.com>
2022-12-09 14:24:40 -0800

    Using if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/**
- * main - Entry point
+/*
+ * main - Entry Point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
