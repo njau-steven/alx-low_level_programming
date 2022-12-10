a4399773fc36fe53024abd7882d82449bdfdd8fd
Njau Steven <njau.steven@gmail.com>
2022-12-09 16:07:31 -0800

    Using if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * main - Entry Point
+/**
+ * main - Entry
  *
  * Return: Always 0 (Success)
  */
@@ -13,7 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand()  - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
