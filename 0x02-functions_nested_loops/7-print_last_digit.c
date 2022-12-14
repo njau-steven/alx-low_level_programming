bdbc96b993a5015263e079102e849fe9d6c149b9
Njau Steven <njau.steven@gmail.com>
2022-12-14 12:36:19 -0800

    Print the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @r: input character
+ * Return: Always 0 (success)
+ */
+
+int print_last_digit(int r)
+{
+	int n;
+
+	if (r < 0)
+	{
+		n = -1 * (r % 10);
+	}
+	else
+	{
+		n = (r % 10);
+	}
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
