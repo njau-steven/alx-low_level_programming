66250070d4905ee6a06a974b76d44f48c8c82409
Njau Steven <njau.steven@gmail.com>
2022-12-14 14:30:41 -0800

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Entry point
+ * @r: Input character
+ * @t: input character
+ * Return: Always 0 (Success)
+ */
+
+int add(int r, int t)
+{
+	return (r + t);
+}
