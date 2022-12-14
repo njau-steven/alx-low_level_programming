c6b64f5b0fdbb2f63240a780567c35279795ea8c
Njau Steven <njau.steven@gmail.com>
2022-12-14 12:17:24 -0800

    Absolute figure

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute figure
+ * @r: inpute character
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+	return (r * -1);
+	}
+}
