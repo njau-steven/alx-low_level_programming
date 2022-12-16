b1c7d8a1f2e0b337f32b3b169a4e602877e43d79
Njau Steven <njau.steven@gmail.com>
2022-12-16 05:53:14 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,10 +15,10 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("Fizz Buzz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
