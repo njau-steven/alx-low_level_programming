5e0ee0dc44b4df71f9e3ebc54b4f99cd244d092c
Njau Steven <njau.steven@gmail.com>
2022-12-16 06:14:39 -0800

    Print traingles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ * @size: input character
+ * Return: Void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, n = size - 1;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+				_putchar(' ');
+				else
+				_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
