f86fc68762a1aaf417c107b6ff49834577c63b44
Njau Steven <njau.steven@gmail.com>
2022-12-16 00:11:52 -0800

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry Point
+ * @a: input character
+ * @b: input character
+ * Return: Always 0 Success
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
