1ed0599b528cad9a6891006bcfb20f01b656119a
Njau Steven <njau.steven@gmail.com>
2023-01-02 00:34:01 -0800

    Int to Pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that takes int to a pointer
+ * @n: an integer input pointer
+ * return: Always 0 Success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
