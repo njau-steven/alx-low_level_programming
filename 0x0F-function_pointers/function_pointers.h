d03d0c446ddaf15e02356927af495360fe4deb4b
Njau Steven <njau.steven@gmail.com>
2023-01-22 01:51:31 -0800

    Function to print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
+#ifndef _0X0F_
+#define _0X0F_
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+#endif
