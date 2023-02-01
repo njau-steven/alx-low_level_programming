efe8d30d1b01567e70c5d05dc1bc84568c2f9a6a
Njau Steven <njau.steven@gmail.com>
2023-02-01 11:04:56 -0800

    Function that add nodes

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - A function that add nodes elements to a linked lists
+ * @head: pointer
+ * @str: pointer
+ * Return: Nothing
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	int length = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	while (str[length])
+		length++;
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
